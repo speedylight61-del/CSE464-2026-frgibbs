092fc9146bb80e276b097a09639415fc7edd3365
speedylight61-del <speedylight61@gmail.com>
2026-03-06 18:11:04 -0700

    Feature 3: addEdge method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,14 @@ public class Main {
         try {
             Graph graph = GraphParser.parseGraph("input.dot");
             graph.addNode("e");
-            graph.addNodes(new String[]{"f", "g", "a"});
+            graph.addNodes(new String[]{"f","g"});
+            graph.addEdge("e","f");
+            graph.addEdge("f","g");
             System.out.println(graph.toString());
 
             graph.outputGraph("graphOutput.txt");
             System.out.println("Graph saved to graphOutput.txt");
+
         } catch (Exception e) {
             System.out.println("Error");
             e.printStackTrace();
